90723c1f30e0ccd9c34a18b8502d3ebcbda4392b
Mark Ogata <ogata@berkeley.edu>
2026-02-16 17:25:03 -0800

    .

--- hw2/experiment2.sh
+++ hw2/experiment2.sh
@@ -8,4 +8,4 @@ uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg
 --discount 0.95 -lr 0.01 --use_baseline -blr 0.01 -bgs 5 --exp_name cheetah_baseline --video_log_freq 10 
 # Baseline with gradients steps 2 instead of 5 experiment
 uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg \
---discount 0.95 -lr 0.01 --use_baseline -blr 0.01 -bgs 2 --exp_name cheetah_baseline_bgs_2
+--discount 0.95 -lr 0.01 --use_baseline -blr 0.01 -bgs 2 --exp_name cheetah_baseline_bgs_2 --video_log_freq 10 
